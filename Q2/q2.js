76bf0fade3643c5886c72c1d3a1b04b77a686eab
azinjmlifr <azinjamalifar@gmail.com>
2022-06-30 15:23:35 +0430

    add q2

---
+++ Q2/q2.js
@@ -0,0 +1,12 @@
+$("select")
+  .change(function () {
+    // $('#paragraph').text($.text());
+    // $('#paragraph').text($('#opt-2').text());
+    // $('#paragraph').text($('#opt-3').text());
+    let selectedText = "";
+    $("select option:selected").each(function () {
+      selectedText += $(this).text() + "";
+    });
+    $("#paragraph").html(selectedText);
+  })
+  .trigger("change");
